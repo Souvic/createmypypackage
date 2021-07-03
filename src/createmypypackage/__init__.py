e77fa04629bd3d76eb41ee77c5df0fd433037396
Souvic Chakraborty <chakra.souvic@gmail.com>
2021-07-03 20:13:45 +0530

    changed os.system to subprocess

--- src/createmypypackage/__init__.py
+++ src/createmypypackage/__init__.py
@@ -1,5 +1,9 @@
 import os
+import subprocess
 import tempfile
+
+def exec(x):
+    subprocess.check_output(x,shell=True)
 
 
 def create_package():
@@ -10,8 +14,8 @@ def create_package():
         extra_file_paths=[]
     with tempfile.TemporaryDirectory() as tmpdirname:
         os.chdir(tmpdirname)
-        os.system("git clone https://github.com/Souvic/package_creator.git")
-        os.system("rm -rf ./package_creator/.git")
+        exec("git clone https://github.com/Souvic/package_creator.git")
+        exec("rm -rf ./package_creator/.git")
 
         package_name=input("Choose name of this package please: ")
         author_name=input("Author's name please: ")
@@ -24,7 +28,7 @@ def create_package():
             rep_url=rep_url+".git"
         with open("./package_creator/src/package_name/__init__.py","w") as f:
             f.write(open(file_path,"r").read())
-        os.system("pipreqs ./package_creator/")
+        exec("pipreqs ./package_creator/")
         print("install_requires in setup.cfg is filled up with minimum requirements from pipreqs package")
         print("Feel free to change setup.cfg after this in your github repo directly in case of different versions to be used")
         with open("./package_creator/requirements.txt") as f:
@@ -36,21 +40,21 @@ def create_package():
         with open("./package_creator/setup.cfg","w") as f:
             f.write(su)
         for ii in extra_file_paths:
-            os.system(f"cp {ii} ./package_creator/src/package_name/")
+            exec(f"cp {ii} ./package_creator/src/package_name/")
         os.rename("./package_creator/src/package_name","./package_creator/src/"+package_name)
         
-        #os.system("python3 -m twine upload --repository testpypi")
+        #exec("python3 -m twine upload --repository testpypi")
         os.chdir("./package_creator/")
-        os.system("python3 -m build")
-        os.system("git init")
-        os.system("git add -A")
-        #os.system("git add .")
-        os.system("git commit -m 'First commit in the new package'")
+        exec("python3 -m build")
+        exec("git init")
+        exec("git add -A")
+        #exec("git add .")
+        exec("git commit -m 'First commit in the new package'")
         print("Uploading to github")
         print("Get ready with your github username and passtoken")
-        os.system("git branch -M main")
-        os.system(f"git remote add origin {rep_url}")
-        os.system("git push -u origin main")
+        exec("git branch -M main")
+        exec(f"git remote add origin {rep_url}")
+        exec("git push -u origin main")
         print("Done!!")
         print("All set up!!")
      
@@ -61,10 +65,10 @@ def uploadpackage():
         rep_url=input("Copy-paste the link of the github repo of the project please: ")
         if(not rep_url.lower().endswith(".git")):
             rep_url=rep_url+".git"
-        os.system(f"git clone {rep_url}")
+        exec(f"git clone {rep_url}")
         dir_ = [i for i in os.listdir() if os.path.isdir(i)][0]
         os.chdir(dir_)
-        os.system(f"rm -rf ./dist")
+        exec(f"rm -rf ./dist")
         nft_=input("Are you uploading this package to PyPi for the first time? Yes/No:").lower().strip()=="no"
         if(nft_):
             with open("./setup.cfg","r") as f:
@@ -75,18 +79,18 @@ def uploadpackage():
             zz[2][len("version = "):]=new_version_number
             with open("./setup.cfg","r") as f:
                 f.write("\n".join(zz))
-        os.system("python3 -m build")
-        os.system("git add -A")
-        os.system("git commit -m 'First commit in the new package'")
+        exec("python3 -m build")
+        exec("git add -A")
+        exec("git commit -m 'First commit in the new package'")
         if(nft_):
-            os.system(f"git tag v{new_version_number}")
-            os.system(f"git push origin  v{new_version_number}")
+            exec(f"git tag v{new_version_number}")
+            exec(f"git push origin  v{new_version_number}")
         else:
-            os.system("git tag v0.0.1")
-            os.system("git push origin  v0.0.1")
-        os.system("git push -u origin main")
+            exec("git tag v0.0.1")
+            exec("git push origin  v0.0.1")
+        exec("git push -u origin main")
         print("Updated github repo!")
-        os.system("twine upload dist/*")
+        exec("twine upload dist/*")
         print("Updated to PyPi!")
         
 if(input("Do you already have a github repo for the project? Yes/No:").lower().strip()=="yes"):
